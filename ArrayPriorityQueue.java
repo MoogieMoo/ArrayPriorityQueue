e8b01353b1a5a2875347df26773cdea18e97e771
MoogieMoo <yxia1@stuy.edu>
2017-04-19 23:47:12 -0400

    Update ArrayPriorityQueue.java

--- ArrayPriorityQueue.java
+++ ArrayPriorityQueue.java
@@ -30,10 +30,10 @@ public class ArrayPriorityQueue implements PriorityQueue {
 	else {
 	    int i = 0;
 	    //find spot of insetion
-	    while ( (int) queue.get(i) < item ) {
+	    while ( (int) queue.get(i) >= item ) {
 		i++;
 	    }
-	    if ( (int) queue.get(i) >= item ) {
+	    if ( (int) queue.get(i) < item ) {
 		queue.add( i, item );
 		return;
 	    }		
